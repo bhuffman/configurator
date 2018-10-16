e9c15382149a6d99962850ade9a6cd9858d46dfe
“Ben <bhuffman@dnanexus.com>
2018-10-15 20:21:30 -0700

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {compose} from 'ramda';
+import ConfigRoot from 'components/pages/ConfigRoot';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {Route, Switch} from 'react-router-dom';
+import {withRouter} from 'react-router';
+
+import { faBars, faPlus, faMinus, faSearch, faAngleRight, faAngleDown, faFileAlt, faSlidersH, faSignOutAlt, faDatabase, faHome, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faPlus, faMinus, faSearch, faAngleRight, faAngleDown, faFileAlt, faSlidersH, faSignOutAlt, faDatabase, faHome, faTimes)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Hello World
+        <Switch>
+          <Route path='/' component={ConfigRoot} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default compose(
+  withRouter
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore';
+import initialState from './initialState';
+import {ThemeProvider} from 'react-jss'
+import createAtomicTheme from './theme';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const myTheme = createAtomicTheme();
+const store = configureStore(initialState);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <ThemeProvider theme={myTheme}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ThemeProvider>
+  , document.getElementById('root'));
+
